68daa730bcedef30d02db6002605ab20304b948e
Agata Musiuk <agata.musiuk@gmail.com>
2019-11-11 12:28:08 +0100

    Adjusted configuration

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    PORT: 8000,
-    DB: 'mongodb+srv://agat:Perseverance92@cluster0-0aleb.mongodb.net/test?retryWrites=true&w=majority',
+    PORT: process.env.PORT,
+    DB: process.env.DB,
 };
