4a730fa81c161de780111f3f2d5df2492e30f3d8
Agata Musiuk <agata.musiuk@gmail.com>
2019-11-24 14:11:16 +0100

    Add built files

---
+++ client/build/precache-manifest.85465f0af72133a395a80fe3edbbd792.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b325fb1aa32f6ab94e300adf26ca7de8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "817b84cedb10ecaa8a2d",
+    "url": "/static/css/2.bba8c6f5.chunk.css"
+  },
+  {
+    "revision": "c655b8148eee8c5430a0",
+    "url": "/static/css/main.64d995c6.chunk.css"
+  },
+  {
+    "revision": "817b84cedb10ecaa8a2d",
+    "url": "/static/js/2.7484db3f.chunk.js"
+  },
+  {
+    "revision": "c655b8148eee8c5430a0",
+    "url": "/static/js/main.b949ce76.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
